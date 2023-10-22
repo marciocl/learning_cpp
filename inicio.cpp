0740a3adf783662c686609c317a8ce6db9cf0c78
Marcio Cavalcante <omarciocavalcante@gmail.com>
2023-10-22 17:23:56 -0300

    Inclusão de while e if else

--- inicio.cpp
+++ inicio.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 using namespace std;
 
-
 int main () {  
-    cout << "*********************************" << endl; 
+    cout << "*****************" << endl; 
     cout << "*Bem vindo ao jogo da advinhação*" << endl;
-    cout << "*********************************" << endl;
+    cout << "*****************" << endl;
 
-    int numero_secreto = 41;
-    cout << "O Numero Secreto é " << numero_secreto << ", guarde Segredo!" << endl;
+    bool acertou = false;
+    const int NUMERO_SECRETO = 41;
+
+    while (!acertou) {
+        int palpite;
+        cout << "Qual o seu Palpite:" << endl; 
+        cin >> palpite;
+
+        acertou = NUMERO_SECRETO == palpite;
+        bool maior = NUMERO_SECRETO < palpite;
+        cout << "Status acertou: " << acertou << endl; 
+
+        if (acertou) {
+            cout << "Você Acertou";
+        }
+        else if (maior) {
+            cout << "O numero que você chutou é maior do que o numero secreto" << endl;
+        }
+        else {
+            cout << "O numero que você chutou é menor do que o numero secreto" << endl;
+        }
+    }
 }
