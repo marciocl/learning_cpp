e7801dc84620d9c720ae4524c8593324453880f0
Marcio Cavalcante <omarciocavalcante@gmail.com>
2023-10-21 22:47:23 -0300

    first commit

---
+++ inicio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {  
+    cout << "*********************************" << endl; 
+    cout << "*Bem vindo ao jogo da advinhação*" << endl;
+    cout << "*********************************" << endl;
+
+    int numero_secreto = 41;
+    cout << "O Numero Secreto é " << numero_secreto << ", guarde Segredo!" << endl;
+}
